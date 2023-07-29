ba2e9a195525babb21818400a258bc91594e41dc
yogita03 <patilyogita408@gmail.com>
2023-07-29 09:43:18 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -4,28 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-// app.use('/',(req, res, next)=>{
-//   console.log('This always runs');
-//   next();
-// });
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product',(req, res, next) =>{
-  console.log('In the middleware');
-
-   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.use('/product',(req, res, next) => {
-  console.log(req.body);
-  res.redirect('/add-product');
-});
-
-app.use('/',(req, res, next) =>{
-    // console.log('In another middleware'); 
-    res.send('<h1>Hello from express</h1>');
-  });
-
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
